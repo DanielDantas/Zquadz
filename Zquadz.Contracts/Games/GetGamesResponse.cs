9bd45ee838be035591e1355d63ff98d17617eb84
Technagem <dadossan@microsoft.com>
2022-12-22 16:44:58 -0500

    removing namespaces

--- Zquadz.Contracts/Games/GetGamesResponse.cs
+++ Zquadz.Contracts/Games/GetGamesResponse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Zquadz.Contracts.Games.Base;
+﻿using Zquadz.Contracts.Games.Base;
 
 namespace Zquadz.Contracts.Games
 {
