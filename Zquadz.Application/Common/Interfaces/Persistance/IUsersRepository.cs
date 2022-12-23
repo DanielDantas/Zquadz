e0ce0d18c8a14f8b04949e228ca1e30fe1fc293d
Technagem <dadossan@microsoft.com>
2022-12-23 13:21:14 -0500

    updating architecture

---
+++ Zquadz.Application/Common/Interfaces/Persistance/IUsersRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zquadz.Application.Common.Interfaces.Persistance
+{
+    public interface IUsersRepository
+    {
+    }
+}
---
+++ Zquadz.Infrastructure/Persistance/UsersRepository.cs
@@ -0,0 +1,8 @@
+﻿using Zquadz.Application.Common.Interfaces.Persistance;
+
+namespace Zquadz.Infrastructure.Persistance
+{
+    public class UsersRepository : IUsersRepository
+    {
+    }
+}
