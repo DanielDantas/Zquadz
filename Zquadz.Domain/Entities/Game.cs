5303cf0042019cfcd117089da8f93238c45b28e7
Technagem <dadossan@microsoft.com>
2022-12-23 14:25:40 -0500

    created simple entities

---
+++ Zquadz.Domain/Entities/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Zquadz.Domain.Entities
+{
+    public class Game
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string CreatedBy { get; set; } = null!;
+    }
+}
---
+++ Zquadz.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Zquadz.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
