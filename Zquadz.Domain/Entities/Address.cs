b516cd2d8d1c561784d6f957ad1caaf3ad7698da
Technagem <dadossan@microsoft.com>
2022-12-29 13:58:36 -0500

    changes in the entities

---
+++ Zquadz.Domain/Entities/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace Zquadz.Domain.Entities
+{
+    public class Address
+    {
+        public string Country { get; set; } = null!;
+        public string State { get; set; } = null!;
+        public string County { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Street { get; set; } = null!;
+        public string Number { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+    }
+}
--- Zquadz.Domain/Entities/Facility.cs
+++ Zquadz.Domain/Entities/Facility.cs
@@ -1,11 +1,9 @@
 ﻿namespace Zquadz.Domain.Entities
 {
-    public class Game
+    public class Facility
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public Guid FacilityId { get; set; }
         public string Name { get; set; } = null!;
-        public string Type { get; set; } = null!;
         public string Description { get; set; } = null!;
         public Guid CreatedBy { get; set; }
         public Guid LastUpdatedBy { get; set; }
@@ -13,12 +11,9 @@
         public string LastUpdatedByName { get; set; } = null!;
         public DateTimeOffset CreatedOn { get; set; }
         public DateTimeOffset LastUpdated { get; set; }
-        public DateTimeOffset StartTime { get; set; }
-        public DateTimeOffset EndTime { get; set; }
-        public short NumberOfPlayersLimit { get; set; }
-        public IEnumerable<Guid> Players { get; set; } = Enumerable.Empty<Guid>();
         public Address Address { get; set; } = null!;
         public double Longitude = 0!;
         public double Latitude = 0!;
+        public IEnumerable<Guid> Administrators { get; set; } = Enumerable.Empty<Guid>();
     }
 }
